24392c1eb33e117620a9a18e3617e6c29f00cb51
kylep34 <74076509+kylep34@users.noreply.github.com>
2021-05-26 17:36:13 -0400

    HW19 complete
    Completed code for HW 19

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+let nav2 = document.querySelector('#nav2');
+let skillBox = document.querySelector('#skillBox');
+let selectorLine = document.querySelector('.selectorLine');
+
+nav2.addEventListener('click' , scrollSkills , false);
+function scrollSkills(e)
+{
+    skillBox.scrollIntoView({ behavior: 'smooth' });
+}
+
+$(nav2).click(function(){
+    $(selectorLine).animate( {left: '994px'}, 1000 );
+});
